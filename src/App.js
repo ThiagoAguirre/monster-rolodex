15b5e6dee227637afc62822e32be92e318161caa
ThiagoAguirre <thiago.aguirre08@outlook.com>
2025-03-23 16:43:44 -0300

    Refactor App component to class-based structure and implement monster search functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+import  { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+          
+  render(){
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <input 
+          className='search-box' 
+          type="search" 
+          placeholder="search monsters" 
+          onChange={e => {
+            const searchField = e.target.value.toLowerCase();
+            this.setState({ searchField });
+          }}
+        />
+        <h1>Monster Rolodex</h1>
+        {
+          filteredMonsters.map(monster => (
+            <h1 key={monster.id}>
+              {monster.name}
+            </h1>
+          ))
+        }
+
+      <button onClick={() => this.setState()}>Change Text</button>
+      <h1>{this.state.string}</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
